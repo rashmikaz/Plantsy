a79e92390d5753513cff5a10baac98e1a4aa67f8
rashmikaz <phrashmika@gmail.com>
2025-05-06 10:01:25 +0530

    explore button

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -6,6 +6,14 @@ const main = () => {
      <div className='w-200 relative top-40 pl-30'>
        <h1 className='text-8xl font-semibold'>Fresh Greens</h1>
        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+       <a 
+          href="#" 
+          role="button"
+          aria-label="View References"
+          className="py-3 px-4 mx-3 rounded-md border relative top-7 right-3"
+        >
+          Explore
+        </a>
 
      </div>
     </div>
