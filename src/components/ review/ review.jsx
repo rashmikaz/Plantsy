d88405105fbbb17331f99d16ae8d6eb3c6f55532
rashmikaz <phrashmika@gmail.com>
2025-05-12 04:00:11 +0530

    review

---
+++ src/components/ review/ review.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import "./review.css";
+
+
+const review = () => {
+    return (
+        <div className="glass-card">
+          <div className="flex flex-col items-center text-center space-y-4">
+        <img
+          src="src/assets/review-01.jpg"
+          alt="Alena Patel"
+          className="w-20 h-20 rounded-full object-cover border-2 border-white/30 relative right-27 top-8"
+        />
+        <div className="relative left-5 bottom-15">
+        <h3 className="text-xl font-semibold">alena Patel</h3>
+        <div className="flex text-yellow-400 text-lg relative ">
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>★</span>
+          <span>☆</span> 
+        </div>
+        </div>
+        <p className="text-white/80 text-sm w-70 relative bottom-10">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt...
+        </p>
+      </div>
+        </div>
+      );
+    };
+    
+export default review
