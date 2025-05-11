009111ba263624e893cc0f2052b6dd1618c40db7
rashmikaz <phrashmika@gmail.com>
2025-05-12 04:25:22 +0530

    trendy plant card

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import Navbar from "./components/navbar";
 import Main from './components/main';
+import Trendy from "./components/trendy/trendy.jsx"
 // import background from "./assets/background.jpg"
 
 
@@ -13,6 +14,7 @@ function App() {
    
       <Navbar />
       <Main />
+      <Trendy />
     
 
          
---
+++ src/components/trendy/trendy.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./trendy.css";
+
+const trendy = () => {
+  return (
+    <div className="relative bottom-40">
+      <h1 className="relative bottom-45 left-120 text-6xl">Our Trendy plants</h1>
+      <div className="glass-card-1">
+          <div className="flex flex-col items-center text-center space-y-4">
+       
+        
+      
+         </div>
+      </div>
+    </div>
+  )
+}
+
+export default trendy
