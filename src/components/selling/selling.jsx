dfd7b3eb2a352a1d726ce499d807302646781341
rashmikaz <phrashmika@gmail.com>
2025-05-13 09:11:52 +0530

    selling card position done

--- src/components/selling/selling.jsx
+++ src/components/selling/selling.jsx
@@ -6,22 +6,96 @@ const selling = () => {
   return (
    <div>
     <h1 className="relative bottom-25 left-120 text-6xl">Our Top Selling</h1>
+
      <div className="selling-card">
-        
      <div className="flex flex-col items-center text-center space-y-4">
-     <img src="src/assets/selling-01.png" alt="" className="relative bottom-37 size-90 " />
-     <h2 className="text-xl relative bottom-45 right-10 font-thin">Calathea plant</h2>
-     <p className="w-61 relative bottom-46 right-4 text-sm font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
-     <h2 className="text-xl relative bottom-45 right-16 font-thin">Rs. 359/-</h2>
+     <img src="src/assets/selling-01.png" alt="" className="relative bottom-48 size-110 " />
+     <h2 className="text-2xl relative bottom-58 right-15 font-thin">Calathea plant</h2>
+     <p className="w-61 relative bottom-57 right-10 text-base font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+     <h2 className="text-2xl relative bottom-50 right-22 font-thin">Rs. 359/-</h2>
     
-     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-57 left-18">
+     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-62 left-18">
      <HiOutlineShoppingBag className="text-xl" />
      </button>
-     
+     </div>
+     </div>
 
- 
+     <div className="relative bottom-110 left-105">
+     <div className="selling-card">
+     <div className="flex flex-col items-center text-center space-y-4">
+     <img src="src/assets/selling-01.png" alt="" className="relative bottom-48 size-110 " />
+     <h2 className="text-2xl relative bottom-58 right-15 font-thin">Calathea plant</h2>
+     <p className="w-61 relative bottom-57 right-10 text-base font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+     <h2 className="text-2xl relative bottom-50 right-22 font-thin">Rs. 359/-</h2>
+    
+     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-62 left-18">
+     <HiOutlineShoppingBag className="text-xl" />
+     </button>
      </div>
      </div>
+     </div>
+
+     <div className="relative bottom-220 left-210">
+     <div className="selling-card">
+     <div className="flex flex-col items-center text-center space-y-4">
+     <img src="src/assets/selling-01.png" alt="" className="relative bottom-48 size-110 " />
+     <h2 className="text-2xl relative bottom-58 right-15 font-thin">Calathea plant</h2>
+     <p className="w-61 relative bottom-57 right-10 text-base font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+     <h2 className="text-2xl relative bottom-50 right-22 font-thin">Rs. 359/-</h2>
+    
+     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-62 left-18">
+     <HiOutlineShoppingBag className="text-xl" />
+     </button>
+     </div>
+     </div>
+     </div>
+
+     <div className="relative bottom-190">
+     <div className="selling-card">
+     <div className="flex flex-col items-center text-center space-y-4">
+     <img src="src/assets/selling-01.png" alt="" className="relative bottom-48 size-110 " />
+     <h2 className="text-2xl relative bottom-58 right-15 font-thin">Calathea plant</h2>
+     <p className="w-61 relative bottom-57 right-10 text-base font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+     <h2 className="text-2xl relative bottom-50 right-22 font-thin">Rs. 359/-</h2>
+    
+     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-62 left-18">
+     <HiOutlineShoppingBag className="text-xl" />
+     </button>
+     </div>
+     </div>
+     </div>
+
+     <div className="relative bottom-300 left-105">
+     <div className="selling-card">
+     <div className="flex flex-col items-center text-center space-y-4">
+     <img src="src/assets/selling-01.png" alt="" className="relative bottom-48 size-110 " />
+     <h2 className="text-2xl relative bottom-58 right-15 font-thin">Calathea plant</h2>
+     <p className="w-61 relative bottom-57 right-10 text-base font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+     <h2 className="text-2xl relative bottom-50 right-22 font-thin">Rs. 359/-</h2>
+    
+     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-62 left-18">
+     <HiOutlineShoppingBag className="text-xl" />
+     </button>
+     </div>
+     </div>
+     </div>
+
+     <div className="relative bottom-410 left-210">
+     <div className="selling-card">
+     <div className="flex flex-col items-center text-center space-y-4">
+     <img src="src/assets/selling-01.png" alt="" className="relative bottom-48 size-110 " />
+     <h2 className="text-2xl relative bottom-58 right-15 font-thin">Calathea plant</h2>
+     <p className="w-61 relative bottom-57 right-10 text-base font-thin">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+     <h2 className="text-2xl relative bottom-50 right-22 font-thin">Rs. 359/-</h2>
+    
+     <button className="border border-white/120 text-white p-1 rounded-md hover:bg-white/10 transition relative bottom-62 left-18">
+     <HiOutlineShoppingBag className="text-xl" />
+     </button>
+     </div>
+     </div>
+     </div>
+
+
    </div>
   )
 }
