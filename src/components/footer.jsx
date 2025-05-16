b8360619d43fd056c564c2e66790e6a5151e8cc8
rashmikaz <phrashmika@gmail.com>
2025-05-16 09:47:50 +0530

    add contact footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/logo.png"
 const footer = () => {
   return (
     <div>
-      <div className="bg-[#222C1D] h-100 ">
+      <div className="bg-[#222C1D] h-90 ">
         <div className="flex items-center flex-shrink-0 relative left-13 top-10">
                 <img className="h-15 w-15 mr-2 relative top-5" src={logo} alt="Logo" />
                 <span className="text-3xl tracking-tight font-bold relative right-3 top-8">Plantsy.</span>
@@ -12,6 +12,12 @@ const footer = () => {
 
         <div className="w-80 relative left-17 top-23">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
+
+        <div class="grid auto-cols-[minmax(0,0.05fr)] grid-flow-col relative left-17 top-35 text-1xl font-bold">
+            <div>FB</div>
+            <div>TW</div>
+            <div>LI</div>
         </div>
 
       </div>
